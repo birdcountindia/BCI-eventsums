b3a20637db556a6270f7f5ae18e1ed7f949a4d93
birdcountindia <skimmer@birdcount.in>
2022-12-19 17:45:47 +0530

    removing Faithful and adding more options for event summaries

--- app.R
+++ app.R
@@ -13,39 +13,45 @@ ui <- fluidPage(
     
     # Sidebar panel for inputs ----
     sidebarPanel(
-      
-      # Input: Slider for the number of bins ----
-      sliderInput(inputId = "bins",
-                  label = "Number of bins:",
-                  min = 1,
-                  max = 50,
-                  value = 30),
-      
+
       # Input: Date selection (value always yyyy-mm-dd, even if display format different) ----
       helpText(h4("Select the start and end dates of interest")),
       
       dateInput(inputId = "startdate", 
                 label = "Start date", 
-                value = "2023-01-01", format = "dd/mm/yy"),
+                value = "2022-11-01", format = "dd/mm/yy"),
       
       dateInput(inputId = "enddate", 
                 label = "End date", 
-                value = "2023-01-04", format = "dd/mm/yy")
+                value = "2022-11-04", format = "dd/mm/yy"),
       
+      # Input: Event code for save file name ----
+      helpText(h4("Provide a short event code (for file downloads)")),
+      textInput(inputId = "event_code", 
+                value = "event_code", label = NULL), 
       
+      # Input: download button ----
+      helpText(h4("Download your data!")),
+      downloadButton("downloadData", "Download",
+                     label = "Data summary"),
+      downloadButton("downloadViz", "Download",
+                     label = "Visual summary")
+
     ),
     
     # Main panel for displaying outputs ----
     mainPanel(
-      
-      # Output: Histogram ----
-      plotOutput(outputId = "distPlot"),
-      
+
       # Output: Lists per day ----
-      plotOutput(outputId = "sumPlot")
+      plotOutput(outputId = "sumPlot"),
+      
+      # Output: Summary table ----
+      tableOutput("table")
       
     )
+    
   )
+
 )
 
 
@@ -55,36 +61,15 @@ server <- function(input, output) {
   
   library(tidyverse)
   library(lubridate)
+  library(glue)
 
   # importing latest month's EBD data
   # this app's directory should be at same level as ebird-datasets/
   load("../ebird-datasets/EBD/ebd_IN_relNov-2022_NOV.RData")
-  
-  
-  # Histogram of the Old Faithful Geyser Data ----
-  # with requested number of bins
-  # This expression that generates a histogram is wrapped in a call
-  # to renderPlot to indicate that:
-  #
-  # 1. It is "reactive" and therefore should be automatically
-  #    re-executed when inputs (input$bins) change
-  # 2. Its output type is a plot
-  
-  output$distPlot <- renderPlot({
-    
-    x    <- faithful$waiting
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-    
-    hist(x, breaks = bins, col = "#007bc2", border = "white",
-         xlab = "Waiting time to next eruption (in mins)",
-         main = "Histogram of waiting times")
-    
-  })
 
   
-  # Summary visualisation of number of lists per day ----
-  
-  output$sumPlot <- renderPlot({
+  # Reactive value for selected dataset (filter, summarise) ----
+  data_sum <- reactive({
     
     # startdate <- "2022-11-01"
     # enddate <- "2022-11-04"
@@ -101,12 +86,39 @@ server <- function(input, output) {
              DAY.M.START = day(DATE.START),
              DAY.M.END = day(DATE.END))
     
-    data_filt <- data_mc %>% 
+    # the output for this data_filt object
+    data_mc %>% 
       # time filter
       filter(YEAR %in% filt_time$YEAR.START:filt_time$YEAR.END,
              MONTH %in% filt_time$MONTH.START:filt_time$MONTH.END,
-             DAY.M %in% filt_time$DAY.M.START:filt_time$DAY.M.END)
+             DAY.M %in% filt_time$DAY.M.START:filt_time$DAY.M.END) %>% 
+      group_by(DAY.M) %>% 
+      summarise(NO.LISTS = n_distinct(GROUP.ID))
     
+  })
+  
+  # Table of selected dataset ----
+  output$table <- renderTable({
+    
+    data_sum()
+      
+  })
+  
+  # Downloadable csv of selected dataset ----
+  output$downloadData <- downloadHandler(
+    
+    filename = function(){glue("{input$event_code}_data.csv")},
+    content = function(file) {
+      write_csv(data_sum(), file)
+    }
+    
+  )
+
+  
+  # Summary visualisation of number of lists per day ----
+  
+  output$sumPlot <- renderPlot({
+
     # plotting 
     data_filt %>% 
       group_by(DAY.M) %>% 
@@ -117,6 +129,17 @@ server <- function(input, output) {
     
     
   })
+  
+  # Download maps ----
+  output$downloadViz <- downloadHandler(
+    
+    filename = function(){glue("{input$event_code}_map1.png")},
+    content = function(file) {
+      (sumPlot()) %>% 
+        ggsave(file)
+    }
+    
+  )
   
 }
 
