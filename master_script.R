a3749ffe3b66150b6bbb129ea9b9d606b925af36
birdcountindia <skimmer@birdcount.in>
2023-01-24 12:32:00 +0530

    automating events analyses: first main commit

---
+++ master_script.R
@@ -0,0 +1,16 @@
+
+
+
+source("scripts/01_param.R")
+
+source("scripts/02_anyevent.R")
+
+if (anyevent == T) {
+  
+  
+  
+} else {
+  
+  print("There are no events that need summaries this time. Quitting from script.")
+  
+}
