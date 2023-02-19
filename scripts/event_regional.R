2c2949691e6f02dc89faadfb05d8158d4e85e9f7
birdcountindia <skimmer@birdcount.in>
2023-02-19 18:40:01 +0530

    fully adapted all analyses of AV's eventsums for regional events

--- scripts/event_regional.R
+++ scripts/event_regional.R
@@ -8,12 +8,14 @@ require(mapview)
 require(leaflet)
 # require(rmapshaper)
 require(writexl)
+require(ggthemes)
 
 
 # paths
 cur_outpath <- glue("outputs/{cur_event$SHORT.CODE}/{cur_event$EDITION)}/")
 
-
+source("https://raw.githubusercontent.com/birdcountindia/bci-functions/main/summaries.R")
+source("https://raw.githubusercontent.com/birdcountindia/bci-functions/main/mapping.R")
 
 # preparing data ----------------------------------------------------------
 
@@ -26,6 +28,7 @@ if (cur_event$SHORT.CODE == "BiBC"){
     filter(STATE.CODE == "IN-AS")
   
   cur_dists_sf <- dists_sf %>% filter(STATE.NAME == "Assam")
+  cur_states_sf <- states_sf %>% filter(STATE.NAME == "Assam")
   
   
   regions <- dists_sf %>% 
@@ -65,7 +68,11 @@ if (cur_event$SHORT.CODE == "BiBC"){
                                DISTRICT.NAME %in% c("Lakhimpur","Majuli","Jorhat",
                                                     "Sivasagar","Dhemaji","Dibrugarh",
                                                     "Charaideo","Tinsukia")
-                               ~ "East"))
+                               ~ "East")) %>% 
+    mutate(REGION1 = factor(REGION1, 
+                            levels = c()))
+  
+  no_regions <- n_distinct(regions$REGION1)
   
   
 } else if (cur_event$SHORT.CODE == "PBC"){
@@ -79,6 +86,7 @@ if (cur_event$SHORT.CODE == "BiBC"){
   cur_dists_sf <- dists_sf %>% 
     filter(STATE.NAME %in% c("Tamil Nadu", "Puducherry"),
            !(DISTRICT.NAME %in% c("Mahe","Yanam")))
+  cur_states_sf <- states_sf %>% filter(STATE.NAME == "Tamil Nadu")
   
   
   regions <- dists_sf %>% 
@@ -103,6 +111,8 @@ if (cur_event$SHORT.CODE == "BiBC"){
                                                     "Puducherry","Karaikal")
                                ~ "East"))
   
+  no_regions <- n_distinct(regions$REGION1)
+  
 }
 
 
@@ -122,7 +132,6 @@ rm(temp)
 # stats -----------------------------------------------------------
 
 # overall stats
-source("https://raw.githubusercontent.com/birdcountindia/bci-functions/main/summaries.R")
 overall_stats <- basic_stats(data0)
 
 # top 10 checklist uploaders (eligible list filter)
@@ -176,44 +185,6 @@ write_xlsx(x = list("Overall stats" = overall_stats,
 
 
 
-# plot districtwise stats on map ----------------------------------------------------
-
-dist_stats <- data0 %>% group_by(DISTRICT.NAME) %>% basic_stats(prettify = F) %>% 
-  # keeping only necessary
-  dplyr::select(SPECIES, LISTS.ALL, PARTICIPANTS, LOCATIONS) %>% 
-  ungroup() %>% 
-  right_join(dists_sf)
-
-
-# different breakpoints in visualisation
-if (cur_event$SHORT.CODE == "BiBC"){
-  
-  break_at <- rev(c(0, 5, 10, 20, 30, max(na.omit(dist_stats$CHECKLISTS))))
-  
-} else if (cur_event$SHORT.CODE == "PBC"){
-  
-  break_at <- rev(c(0, 10, 50, 100, 250, max(na.omit(dist_stats$CHECKLISTS))))
-  
-}
-
-
-mapviewOptions(fgb = FALSE)
-map_effort <- mapView(effortmap, 
-                      zcol = c("CHECKLISTS"), 
-                      map.types = c("Esri.WorldImagery"),
-                      layer.name = c("CHECKLISTS - DISTRICTS"), 
-                      popup = leafpop::popupTable(dist_stats,
-                                                  zcol = c("DISTRICT.NAME", "CHECKLISTS",
-                                                           "PARTICIPANTS", "LOCATIONS", "SPECIES"), 
-                                                  feature.id = FALSE,
-                                                  row.numbers = FALSE),
-                      at = break_at, 
-                      alpha.regions = 0.6)
-mapshot(map_effort, 
-        file = glue("{cur_outpath}{cur_event$FULL.CODE}_dists_effort.html"))
-
-
-
 # regional summaries and common species -----------------------------------
 
 reg_stats <- data0 %>% 
@@ -250,4 +221,108 @@ com_spec <- data0 %>%
 write_xlsx(x = list("Stats" = reg_stats, 
                     "Common species" = com_spec),
            path = glue("{cur_outpath}{cur_event$FULL.CODE}_regions.xlsx"))
+
+
+
+# plot districtwise stats on map ----------------------------------------------------
+
+dist_stats <- data0 %>% group_by(DISTRICT.NAME) %>% basic_stats(prettify = F) %>% 
+  # keeping only necessary
+  dplyr::select(SPECIES, LISTS.ALL, PARTICIPANTS, LOCATIONS) %>% 
+  ungroup() %>% 
+  right_join(dists_sf)
+
+
+# different breakpoints in visualisation
+if (cur_event$SHORT.CODE == "BiBC"){
+  
+  break_at <- rev(c(0, 5, 10, 20, 30, max(na.omit(dist_stats$CHECKLISTS))))
+  
+} else if (cur_event$SHORT.CODE == "PBC"){
+  
+  break_at <- rev(c(0, 10, 50, 100, 250, max(na.omit(dist_stats$CHECKLISTS))))
+  
+}
+
+
+mapviewOptions(fgb = FALSE)
+map_effort <- mapView(effortmap, 
+                      zcol = c("CHECKLISTS"), 
+                      map.types = c("Esri.WorldImagery"),
+                      layer.name = c("CHECKLISTS - DISTRICTS"), 
+                      popup = leafpop::popupTable(dist_stats,
+                                                  zcol = c("DISTRICT.NAME", "CHECKLISTS",
+                                                           "PARTICIPANTS", "LOCATIONS", "SPECIES"), 
+                                                  feature.id = FALSE,
+                                                  row.numbers = FALSE),
+                      at = break_at, 
+                      alpha.regions = 0.6)
+mapshot(map_effort, 
+        file = glue("{cur_outpath}{cur_event$FULL.CODE}_distseffortmap.html"))
+
+
+
+# plot point map ----------------------------------------------------------
+
+point_map <- data0 %>% 
+  distinct(LOCALITY.ID, LATITUDE, LONGITUDE) %>% 
+  cov_point_map_plain(., poly_sf = cur_states_sf, 
+                      poly_bound_col = "white",
+                      point_size = 1, point_alpha = 0.3)
+
+ggsave(filename = glue("{cur_outpath}{cur_event$FULL.CODE}_pointmap.png"), 
+       plot = point_map, 
+       device = png, units = "in", width = 8, height = 8, bg = "black", dpi = 300)
+
+
+
+# plot point map on districts ---------------------------------------------
+
+point_dist_map <- data0 %>% 
+  distinct(LOCALITY.ID, LATITUDE, LONGITUDE) %>% 
+  cov_point_map_plain(., poly_sf = cur_dists_sf, 
+                      poly_bound_col = "black", plot_fill = "white", 
+                      point_col = "blue", point_size = 1, point_alpha = 0.3)
+
+ggsave(filename = glue("{cur_outpath}{cur_event$FULL.CODE}_pointdistmap.png"), 
+       plot = point_dist_map, 
+       device = png, units = "in", width = 8, height = 8, dpi = 300)
+
+
+
+# plot region map ---------------------------------------------------------
+
+theme_set(theme_tufte())
+
+
+palette_vals <- c("#869B27", "#E49B36", "#A13E2B", "#78CAE0", "#B69AC9", "#EA5599", 
+                  "#31954E", "#493F3D", "#CC6666", "#9999CC", "#000000", "#66CC99")[1:no_regions]
+
+
+region_map <- dists_sf %>% 
+  # joining region info
+  left_join(regions) %>% 
+  ggplot() +
+  geom_sf(aes(fill = REGION1, geometry = geometry), colour = NA) +
+  # scale_x_continuous(expand = c(0,0)) +
+  # scale_y_continuous(expand = c(0,0)) +
+  theme(axis.line = element_blank(),
+        axis.text.x = element_blank(),
+        axis.text.y = element_blank(),
+        axis.ticks = element_blank(),
+        axis.title.x = element_blank(),
+        axis.title.y = element_blank(),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_blank(),
+        plot.margin = unit(c(0,0,0,0), "cm"),
+        panel.background = element_blank(),
+        legend.title = element_blank(), 
+        legend.text = element_text(size = 12)) +
+  scale_fill_manual(values = palette_vals)+
+  scale_colour_manual(values = palette_vals) 
+
+ggsave(filename = glue("{cur_outpath}{cur_event$FULL.CODE}_regionmap.png"), 
+       plot = region_map, 
+       device = png, units = "in", width = 10, height = 7, dpi = 300)
 
